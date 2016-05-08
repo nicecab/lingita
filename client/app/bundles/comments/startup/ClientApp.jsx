f5fa1e9fa57ef8f6438ce359db1f6193b68c44ca
Tomoya Hirano <hiranotomoya@gmail.com>
2016-05-08 12:30:08 +0200

    Add Material-UI

--- client/app/bundles/comments/startup/ClientApp.jsx
+++ client/app/bundles/comments/startup/ClientApp.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 
+import injectTapEventPlugin from 'react-tap-event-plugin';
+// Needed for onTouchTap
+// Can go away when react 1.0 release
+// Check this repo:
+// https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
+
 import createStore from '../store/commentsStore';
 import NonRouterCommentsContainer from '../containers/NonRouterCommentsContainer';
 
