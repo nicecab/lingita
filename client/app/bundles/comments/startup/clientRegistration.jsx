838da1253ffc8851ceb886086badda844d7548df
Tomoya Hirano <hiranotomoya@gmail.com>
2016-05-31 21:46:51 +0200

    Material-UI

--- client/app/bundles/comments/startup/clientRegistration.jsx
+++ client/app/bundles/comments/startup/clientRegistration.jsx
@@ -1,7 +1,14 @@
 import App from './ClientApp';
 import RouterApp from './ClientRouterApp';
+import HomeApp from './HomeApp';
 import SimpleCommentScreen from '../components/SimpleCommentScreen/SimpleCommentScreen';
 import ReactOnRails from 'react-on-rails';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// Check this repo:
+// https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
 
 ReactOnRails.setOptions({
   traceTurbolinks: TRACE_TURBOLINKS, // eslint-disable-line no-undef
@@ -11,6 +18,7 @@ ReactOnRails.register(
   {
     App,
     RouterApp,
+    HomeApp,
     SimpleCommentScreen,
   }
 );
